0f13ac4a972597fefe6e12c727f3955ff52c68b2
mdashraful <mdashrafulislam369@gmail.com>
2023-03-27 09:43:12 +0600

    added product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,6 +7,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-body'>
+                <h6 className='product-name'>{name}</h6>
+                <p style={{ marginBottom: '30px' }}>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} star</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
