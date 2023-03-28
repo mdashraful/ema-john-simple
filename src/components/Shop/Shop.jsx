f881b40deaab20a3b3c2076842664bb79138cc55
mdashraful <mdashrafulislam369@gmail.com>
2023-03-29 01:02:10 +0600

    save cart to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import axios, { Axios } from 'axios';
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,7 +13,15 @@ const Shop = () => {
         const newCart = [...cart, product];
 
         setCart(newCart);
+
+        addToDb(product.id);
     }
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+
+        console.log(storedCart);
+    }, []);
 
     useEffect(() => {
         const loadData = async () => {
